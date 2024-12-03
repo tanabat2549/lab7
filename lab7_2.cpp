4b109c1e36cba9964622d4bce7462eaf89176b3b
Thanabat <tanabat2549kuayjeng@gmail.com>
2024-12-03 17:09:19 +0700

    complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -16,3 +16,44 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
+
+
+    int ID;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name << ": ";
+    cin >> ID;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR " << (ID/10000000)-12<<". I have a free movie ticket for you.\n";
+
+
+    string movie;
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n"; 
+    cout << name << ": ";
+    getline(cin,movie);
+    
+     string day;
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name << ": ";
+    getline(cin,day);
+    
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
+
+
+    string text;
+    cout << name << ": ";
+    cin >> text;
+    cin.ignore();
+
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+}
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,18 +2,13 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+	if(x == 65){
+		return 90;
+	}else if(x>90 && x<65){
+		return 48;
+	}else if(x > 65&&x <= 90){
+		return x-1;
+	}else{
+		return 48;
+	}
 }
